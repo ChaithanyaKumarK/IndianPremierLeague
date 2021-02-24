b90ff88705014c6ec650a45e28dbf8492e33d71c
ChaithanyaKumarK <chaithanya.kumar@mountblue.io>
2021-02-24 13:42:41 +0530

    Refactored server.js -urlSwitcher takes the url extensions switches to relevent path and content-type -pathAndContentTypeSetter takes relevent path and content-type and returns the fileData

--- src/server/server.js
+++ src/server/server.js
@@ -6,88 +6,50 @@ import http from 'http'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const server = http.createServer((request, response) => {
-    if (request.url == '/') {
-        response.write(fs.readFileSync(path.resolve(__dirname, '../client/index.html'), 'utf-8', (err, HTMLdata) => {
-            if (err) {
-                return '404';
-            }
-            else {
-                console.log(request.url);
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                return HTMLdata;
-            }
-        }))
-    }
-    else if (request.url == '/charts.js') {
-        response.write(fs.readFileSync(path.resolve(__dirname, '../client/charts.js'), 'utf-8', (err, HTMLdata) => {
-            if (err) {
-                return '404';
-            }
-            else {
-                console.log(request.url);
-                response.writeHead(200, { 'Content-Type': 'application/javascript' });
-                return HTMLdata;
+    const urlReceived = request.url;
 
-            }
-        }))
-
-    }
-    else if (request.url == '/matchesPerYear.json') {
-        response.write(fs.readFileSync(path.resolve(__dirname, '../public/output/matchesPerYear.json'), 'utf-8', (err, HTMLdata) => {
-            if (err) {
-                return '404';
-            }
-            else {
-                console.log(request.url);
-                response.writeHead(200, { 'Content-Type': 'application/json' });
-                return HTMLdata;
-
-            }
-        }))
-
-    }
-    else if (request.url == '/economicalBowlers2015.json') {
-        response.write(fs.readFileSync(path.resolve(__dirname, '../public/output/economicalBowlers2015.json'), 'utf-8', (err, HTMLdata) => {
-            if (err) {
-                return '404';
-            }
-            else {
-                console.log(request.url);
-                response.writeHead(200, { 'Content-Type': 'application/json' });
-                return HTMLdata;
-
-            }
-        }))
-
-    }
-    else if (request.url == '/extraRunPerTeam2016.json') {
-        response.write(fs.readFileSync(path.resolve(__dirname, '../public/output/extraRunPerTeam2016.json'), 'utf-8', (err, HTMLdata) => {
-            if (err) {
-                return '404';
-            }
-            else {
-                console.log(request.url);
-                response.writeHead(200, { 'Content-Type': 'application/json' });
-                return HTMLdata;
-
-            }
-        }))
-
-    }
-    else if (request.url == '/matchesWonPerTeam.json') {
-        response.write(fs.readFileSync(path.resolve(__dirname, '../public/output/matchesWonPerTeam.json'), 'utf-8', (err, HTMLdata) => {
-            if (err) {
-                return '404';
-            }
-            else {
-                console.log(request.url);
-                response.writeHead(200, { 'Content-Type': 'application/json' });
-                return HTMLdata;
-
-            }
-        }))
-
-    }
+    const pathExtension = urlReceived.split('.').splice(-1)[0];
+    urlSwitcher(request, response, pathExtension);
     response.end();
 }).listen(3000);
 console.log('Started 3000');
+
+
+function urlSwitcher(request, response, pathExtension) {
+    const outputFileDirectoryPath = '../public/output';
+    const clientFileDirectoryPath = '../client';
+    let htmlFilePAth;
+
+    switch (pathExtension) {
+        case '/':
+            htmlFilePAth = path.join(__dirname, clientFileDirectoryPath, 'index.html');
+            pathAndContentTypeSetter(response, htmlFilePAth, 'text/html');
+            break;
+        case 'js':
+            htmlFilePAth = path.join(__dirname, clientFileDirectoryPath, request.url);
+            pathAndContentTypeSetter(response, htmlFilePAth, 'application/javascript');
+            break;
+        case 'json':
+            htmlFilePAth = path.join(__dirname, outputFileDirectoryPath, request.url);
+            pathAndContentTypeSetter(response, htmlFilePAth, 'application/json');
+            break;
+    }
+
+}
+
+
+//this function writes 
+function pathAndContentTypeSetter(response, pathOfFile, contentType) {
+    response.write(fs.readFileSync(pathOfFile, 'utf-8', (err, HTMLdata) => {
+        if (err) {
+            return '404';
+        }
+        else {
+            console.log(request.url);
+            response.writeHead(200, { 'Content-Type': contentType });
+            return HTMLdata;
+        }
+
+    }))
+
+}
